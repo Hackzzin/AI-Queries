0dda3095a257e6951c00ac295628692f851fb36a
Hackzzin <matheushackbus@gmail.com>
2025-07-07 19:09:04 -0300

    Added sql_ddl, ontology serialized as .ttl and create genQuery file

---
+++ genQuery.py
@@ -0,0 +1,10 @@
+perguntas = ["Quais são os nomes de todos os departamentos e seus respectivos gerentes?",
+             "Qual é o maior e o menor salário?",
+             "Liste todos os empregados contratados antes de 1986.",
+             "Qual é o intervalo salarial entre os gerentes?",
+             "Quantos cargos diferentes temos?",
+             "Quantos empregados são homens? Quantos são mulheres?",
+             "Liste, por departamento, os cargos e a média salarial dos empregados por cargos.",
+             "Liste, por empregados, os seus departamentos trabalhados e seus respectivos salários na época.",
+             "Liste os gerentes de departamento contratados antes de 1991 e seus salários atuais."]
+
